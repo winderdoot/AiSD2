fbc208dd80437f3f5727268085c774b344c0b290
winderdoot <01189157@pw.edu.pl>
2025-03-20 21:23:28 +0100

    zadanko z labiryntem

--- Lab5/Lab05.cs
+++ Lab5/Lab05.cs
@@ -1,4 +1,4 @@
-﻿//using System.Linq;
+﻿using System.Linq;
 
 /*
  * W tym zadaniu mamy dwa podejścia:
@@ -17,23 +17,86 @@
 
 
 
-//namespace ASD
-//{
-//    using ASD.Graphs;
-//    using System;
-//    using System.Collections.Generic;
+namespace ASD
+{
+    using ASD.Graphs;
+    using System;
+    using System.Collections.Generic;
 
-//    public class Lab06 : System.MarshalByRefObject
-//    {
-//        public List<int> WidePath(DiGraph<int> G, int start, int end)
-//        {
-//            return new List<int>();
-//        }
+    public class WidthComparer : Comparer<int>
+    {
+        public override int Compare(int x, int y)
+        {
+            if (x < y)
+            {
+                return -1;
+            }
+            if (x == y)
+                return 0;
+            return 1;
+        }
+    }
 
 
-//        public List<int> WeightedWidePath(DiGraph<int> G, int start, int end, int[] weights, int maxWeight)
-//        {
-//            return new List<int>();
-//        }
-//    }
-//}
+    public class Lab06 : System.MarshalByRefObject
+    {
+        public List<int> WidePath(DiGraph<int> G, int start, int end)
+        {
+            int N = G.VertexCount;
+
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>(new WidthComparer(), N);
+            int[] smallestWidthInPath = new int[N];
+            int[] cameFrom = new int[N];
+
+            for (int v = 0; v < N; v++)
+            {
+                if (v == start)
+                    continue;
+                queue.Insert(v, int.MaxValue);
+                smallestWidthInPath[v] = -1;
+                cameFrom[v] = -1;
+            }
+            cameFrom[start] = start;
+            smallestWidthInPath[start] = 0;
+            queue.Insert(start, 0);
+            while (queue.Count > 0)
+            {
+                int v = queue.Extract();
+                int vWidth = smallestWidthInPath[v];
+                foreach (int nei in G.OutNeighbors(v))
+                {
+                    int newWidth = Math.Min(vWidth, G.GetEdgeWeight(v, nei));
+                    if (newWidth == 0)
+                    {
+                        newWidth = G.GetEdgeWeight(v, nei);
+                    }
+                    if (newWidth > smallestWidthInPath[nei])
+                    {
+                        smallestWidthInPath[nei] = newWidth;
+                        cameFrom[nei] = v;
+                        queue.Insert(nei, newWidth);
+                    }
+                }
+            }
+            if (smallestWidthInPath[end] == -1)
+            {
+                return null;
+            }
+            List<int> path = new List<int>(N);
+            int x = end;
+            while (cameFrom[x] != x)
+            {
+                path.Add(x);
+                x = cameFrom[x];
+            }
+            path.Reverse();
+            return path;
+        }
+
+
+        public List<int> WeightedWidePath(DiGraph<int> G, int start, int end, int[] weights, int maxWeight)
+        {
+            return new List<int>();
+        }
+    }
+}
--- Lab5/Maze.cs
+++ Lab5/Maze.cs
@@ -1,7 +1,12 @@
 ﻿using ASD.Graphs;
 using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
+using System.Text.Json.Serialization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 /*
@@ -16,6 +21,41 @@ namespace ASD
     public class Maze : MarshalByRefObject
     {
 
+        public static int GetMazeIndex(int x, int y, int Width, int Height)
+        {
+            if (x < 0 || x >= Width || y < 0 || y >= Height)
+                return -1;
+            return y * Width + x;
+        }
+
+        public static string ReconstructPath(PathsInfo<int> pinfo, int source, int exit, int Width, int Height)
+        {
+            StringBuilder sb = new StringBuilder();
+            var path = pinfo.GetPath(source, exit);
+            int last = path[0];
+            for (int i = 1; i < path.Length; i++)
+            {
+                if (path[i] == last + 1)
+                {
+                    sb.Append('E');
+                }
+                else if (path[i] == last - 1)
+                {
+                    sb.Append('W');
+                }
+                else if (path[i] == last - Width)
+                {
+                    sb.Append('N');
+                }
+                else if (path[i] == last + Width)
+                {
+                    sb.Append('S');
+                }
+                last = path[i];
+            }
+            return sb.ToString();
+        }
+
         /// <summary>
         /// Wersje zadania I oraz II
         /// Zwraca najkrótszy możliwy czas przejścia przez labirynt bez dynamitów lub z dowolną ich liczbą
@@ -27,9 +67,95 @@ namespace ASD
         /// <param name="t">czas zburzenia ściany (dotyczy tylko wersji II)</param> 
         public int FindShortestPath(char[,] maze, bool withDynamite, out string path, int t = 0)
         {
-            path = "";
+            int Height = maze.GetLength(0);
+            int Width = maze.GetLength(1);
+            int source = -1, exit = -1;
+            //Console.WriteLine();
 
-            return -1;
+            //for (int x = 0; x < Height; x++)
+            //{
+            //    for (int y = 0; y < Width; y++)
+            //    {
+            //        Console.Write(maze[x, y]);
+            //    }
+            //    Console.WriteLine(); 
+            //}
+
+            DiGraph<int> g = new DiGraph<int>(Width * Height);
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    int from = GetMazeIndex(x, y, Width, Height);
+                    if (maze[y, x] == 'S')
+                        source = from;
+                    else if (maze[y, x] == 'E')
+                        exit = from;
+                    (int, int)[] neighbors = new (int, int)[4] { (x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1) };
+                    foreach ((int toX, int toY) in  neighbors)
+                    {
+                        int to = GetMazeIndex(toX, toY, Width, Height);
+                        if (to == -1)
+                            continue;
+                        if (maze[toY, toX] == 'X' && withDynamite)
+                        {
+                            g.AddEdge(from, to, t);
+                        }
+                        else if (maze[toY, toX] == 'O' || maze[toY, toX] == 'E' || maze[toY, toX] == 'S')
+                        {
+                            g.AddEdge(from, to, 1);
+                        }
+                    }
+                }   
+            }
+            PathsInfo<int> pinfo = Paths.Dijkstra(g, source);
+            if (!pinfo.Reachable(source, exit))
+            {
+                path = "";
+                return -1;
+            }
+
+            path = ReconstructPath(pinfo, source, exit, Width, Height);
+            return pinfo.GetDistance(source, exit);
+        }
+
+        public static int GetMazeIndexLeveled(int x, int y, int level, int Width, int Height)
+        {
+            if (x < 0 || x >= Width || y < 0 || y >= Height)
+                return -1;
+            return y * Width + x + Width * Height * level;
+        }
+
+        public static int GetVertexLevel(int ind, int Width, int Height)
+        {
+            return ind / Width * Height;
+        }
+        public static string ReconstructPathLeveled(PathsInfo<int> pinfo, int source, int exit, int Width, int Height, int k)
+        {
+            StringBuilder sb = new StringBuilder();
+            var path = pinfo.GetPath(source, exit);
+            int last = path[0];
+            for (int i = 1; i < path.Length - 1; i++)
+            {
+                if (path[i] == last + 1 || (path[i] == last + 1 + Width * Height ))
+                {
+                    sb.Append('E');
+                }
+                else if (path[i] == last - 1 || path[i] == last - 1 + Width * Height)
+                {
+                    sb.Append('W');
+                }
+                else if (path[i] == last - Width || path[i] == last - Width + Width * Height)
+                {
+                    sb.Append('N');
+                }
+                else if (path[i] == last + Width || path[i] == last + Width + Width * Height)
+                {
+                    sb.Append('S');
+                }
+                last = path[i];
+            }
+            return sb.ToString();
         }
 
         /// <summary>
@@ -42,8 +168,67 @@ namespace ASD
         /// <param name="t">czas zburzenia ściany</param>
         public int FindShortestPathWithKDynamites(char[,] maze, int k, out string path, int t)
         {
-            path = "";
-            return -1;
+            int Height = maze.GetLength(0);
+            int Width = maze.GetLength(1);
+            int source = -1, exit = Width * Height * (k + 1);
+            int[] allExits = new int[k + 1];
+            Array.Fill(allExits, -1);
+
+            //Console.WriteLine();
+
+            //for (int x = 0; x < Height; x++)
+            //{
+            //    for (int y = 0; y < Width; y++)
+            //    {
+            //        Console.Write(maze[x, y]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            DiGraph<int> g = new DiGraph<int>(Width * Height * (k + 1) + 1);
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    for (int z = 0; z <= k; z++) // Amount of dynamite used
+                    {
+                        int from = GetMazeIndexLeveled(x, y, z, Width, Height);
+                        if (z == 0 && maze[y, x] == 'S')
+                            source = from;
+                        else if (maze[y, x] == 'E')
+                            allExits[z] = from; 
+                        (int, int)[] neighbors = new (int, int)[4] { (x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1) };
+                        foreach ((int toX, int toY) in neighbors)
+                        {
+                            int to = GetMazeIndexLeveled(toX, toY, z, Width, Height);
+                            if (to == -1)
+                                continue;
+                            if (maze[toY, toX] == 'X' && z < k)
+                            {
+                                g.AddEdge(from, to + Width * Height, t);
+                            }
+                            else if (maze[toY, toX] == 'O' || maze[toY, toX] == 'E' || maze[toY, toX] == 'S')
+                            {
+                                g.AddEdge(from, to, 1);
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < allExits.Length; i++)
+            {
+                g.AddEdge(allExits[i], exit, 0);
+            }
+            PathsInfo<int> pinfo = Paths.Dijkstra(g, source);
+            if (!pinfo.Reachable(source, exit))
+            {
+                path = "";
+                return -1;
+            }
+            path = ReconstructPathLeveled(pinfo, source, exit, Width, Height, k);
+            //Console.WriteLine(path);
+
+            return pinfo.GetDistance(source, exit);
         }
     }
 }
