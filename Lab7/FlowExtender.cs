1c61dbde937e184ceb20260e955b425f09fce75d
winderdoot <01189157@pw.edu.pl>
2025-04-03 11:16:34 +0200

    zadanie 7-1 slabe testy

---
+++ Lab7/FlowExtender.cs
@@ -0,0 +1,78 @@
+﻿using ASD.Graphs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASD
+{
+    public static class FlowExtender
+    {
+
+        /// <summary>
+        /// Metoda wylicza minimalny s-t-przekrój grafu nieskierowanego.
+        /// </summary>
+        /// <param name="undirectedGraph">Nieskierowany graf</param>
+        /// <param name="s">wierzchołek źródłowy</param>
+        /// <param name="t">wierzchołek docelowy</param>
+        /// <param name="minCut">minimalny przekrój</param>
+        /// <returns>wartość przekroju</returns>
+        public static double MinCut(this Graph<double> undirectedGraph, int s, int t, out Edge<double>[] minCut)
+        {
+            int N = undirectedGraph.VertexCount;
+            DiGraph<double> network = new DiGraph<double>(N, undirectedGraph.Representation);
+            foreach (Edge<double> e in undirectedGraph.DFS().SearchFrom(s))
+            {
+                network.AddEdge(e.From, e.To, e.Weight);
+                network.AddEdge(e.To, e.From, e.Weight);
+            }
+            (double flowVal, DiGraph<double> maxFlow) = Flows.FordFulkerson<double>(network, s, t);
+            DiGraph<double> residual = new DiGraph<double>(N, maxFlow.Representation);
+            foreach (Edge<double> e in network.DFS().SearchFrom(s))
+            {
+                double cap = undirectedGraph.GetEdgeWeight(e.From, e.To);
+                double flow = maxFlow.HasEdge(e.From, e.To) ? maxFlow.GetEdgeWeight(e.From, e.To) : 0;
+                double extraFlow = cap - flow;
+                if (extraFlow > 0)
+                {
+                    residual.AddEdge(e.From, e.To, extraFlow);
+                }
+                if (flow > 0)
+                {
+                    residual.AddEdge(e.To, e.From, flow);
+                }
+            }
+            bool[] sReachable = new bool[N];//Can be reached from source in the residual network
+            foreach (Edge<double> e in residual.DFS().SearchFrom(s))
+            {
+                //sReachable[e.From] = true;
+                sReachable[e.To] = true;
+            }
+            // Teraz znaleźć krawędzie leżące na minimalnym przekroju
+            List<Edge<double>> cut = new();
+            foreach (Edge<double> e in network.DFS().SearchFrom(s))
+            {
+                if (sReachable[e.From] && !sReachable[e.To])
+                {
+                    cut.Add(e);
+                }
+            }
+            minCut = cut.ToArray();
+            return flowVal;
+        }
+
+        /// <summary>
+        /// Metada liczy spójność krawędziową grafu oraz minimalny zbiór rozcinający.
+        /// </summary>
+        /// <param name="undirectedGraph">nieskierowany graf</param>
+        /// <param name="cutingSet">zbiór krawędzi rozcinających</param>
+        /// <returns>spójność krawędziowa</returns>
+        public static int EdgeConnectivity(this Graph<double> undirectedGraph, out Edge<double>[] cutingSet)
+        {
+            cutingSet = null;
+            return 0;
+        }
+        
+    }
+}
---
+++ Lab7/ProductionPlanner.cs
@@ -0,0 +1,99 @@
+﻿//using System;
+//using System.Linq;
+//using ASD.Graphs;
+
+//namespace ASD
+//{
+//    public class ProductionPlanner : MarshalByRefObject
+//    {
+//        /// <summary>
+//        /// Flaga pozwalająca na włączenie wypisywania szczegółów skonstruowanego planu na konsolę.
+//        /// Wartość <code>true</code> spoeoduje wypisanie planu.
+//        /// </summary>
+//        public bool ShowDebug { get; } = false;
+
+//        /// <summary>
+//        /// Część 1. zadania - zaplanowanie produkcji telewizorów dla pojedynczego kontrahenta.
+//        /// </summary>
+//        /// <remarks>
+//        /// Do przeprowadzenia testów wyznaczających maksymalną produkcję i zysk wymagane jest jedynie zwrócenie obiektu <see cref="PlanData"/>.
+//        /// Testy weryfikujące plan wymagają przypisania tablicy z planem do parametru wyjściowego <see cref="weeklyPlan"/>.
+//        /// </remarks>
+//        /// <param name="production">
+//        /// Tablica obiektów zawierających informacje o produkcji fabryki w kolejnych tygodniach.
+//        /// Wartości pola <see cref="PlanData.Quantity"/> oznaczają limit produkcji w danym tygodniu,
+//        /// a pola <see cref="PlanData.Value"/> - koszt produkcji jednej sztuki.
+//        /// </param>
+//        /// <param name="sales">
+//        /// Tablica obiektów zawierających informacje o sprzedaży w kolejnych tygodniach.
+//        /// Wartości pola <see cref="PlanData.Quantity"/> oznaczają maksymalną sprzedaż w danym tygodniu,
+//        /// a pola <see cref="PlanData.Value"/> - cenę sprzedaży jednej sztuki.
+//        /// </param>
+//        /// <param name="storageInfo">
+//        /// Obiekt zawierający informacje o magazynie.
+//        /// Wartość pola <see cref="PlanData.Quantity"/> oznacza pojemność magazynu,
+//        /// a pola <see cref="PlanData.Value"/> - koszt przechowania jednego telewizora w magazynie przez jeden tydzień.
+//        /// </param>
+//        /// <param name="weeklyPlan">
+//        /// Parametr wyjściowy, przez który powinien zostać zwrócony szczegółowy plan sprzedaży.
+//        /// </param>
+//        /// <returns>
+//        /// Obiekt <see cref="PlanData"/> opisujący wyznaczony plan.
+//        /// W polu <see cref="PlanData.Quantity"/> powinna znaleźć się maksymalna liczba wyprodukowanych telewizorów,
+//        /// a w polu <see cref="PlanData.Value"/> - wyznaczony maksymalny zysk fabryki.
+//        /// </returns>
+//        public PlanData CreateSimplePlan(PlanData[] production, PlanData[] sales, PlanData storageInfo,
+//            out SimpleWeeklyPlan[] weeklyPlan)
+//        {
+//            weeklyPlan = null;
+//            return new PlanData
+//            {
+//                Value = 0,
+//                Quantity = 0
+//            };
+//        }
+
+//        /// <summary>
+//        /// Część 2. zadania - zaplanowanie produkcji telewizorów dla wielu kontrahentów.
+//        /// </summary>
+//        /// <remarks>
+//        /// Do przeprowadzenia testów wyznaczających produkcję dającą maksymalny zysk wymagane jest jedynie zwrócenie obiektu <see cref="PlanData"/>.
+//        /// Testy weryfikujące plan wymagają przypisania tablicy z planem do parametru wyjściowego <see cref="weeklyPlan"/>.
+//        /// </remarks>
+//        /// <param name="production">
+//        /// Tablica obiektów zawierających informacje o produkcji fabryki w kolejnych tygodniach.
+//        /// Wartość pola <see cref="PlanData.Quantity"/> oznacza limit produkcji w danym tygodniu,
+//        /// a pola <see cref="PlanData.Value"/> - koszt produkcji jednej sztuki.
+//        /// </param>
+//        /// <param name="sales">
+//        /// Dwuwymiarowa tablica obiektów zawierających informacje o sprzedaży w kolejnych tygodniach.
+//        /// Pierwszy wymiar tablicy jest równy liczbie kontrahentów, zaś drugi - liczbie tygodni w planie.
+//        /// Wartości pola <see cref="PlanData.Quantity"/> oznaczają maksymalną sprzedaż w danym tygodniu,
+//        /// a pola <see cref="PlanData.Value"/> - cenę sprzedaży jednej sztuki.
+//        /// Każdy wiersz tablicy odpowiada jednemu kontrachentowi.
+//        /// </param>
+//        /// <param name="storageInfo">
+//        /// Obiekt zawierający informacje o magazynie.
+//        /// Wartość pola <see cref="PlanData.Quantity"/> oznacza pojemność magazynu,
+//        /// a pola <see cref="PlanData.Value"/> - koszt przechowania jednego telewizora w magazynie przez jeden tydzień.
+//        /// </param>
+//        /// <param name="weeklyPlan">
+//        /// Parametr wyjściowy, przez który powinien zostać zwrócony szczegółowy plan sprzedaży.
+//        /// </param>
+//        /// <returns>
+//        /// Obiekt <see cref="PlanData"/> opisujący wyznaczony plan.
+//        /// W polu <see cref="PlanData.Quantity"/> powinna znaleźć się optymalna liczba wyprodukowanych telewizorów,
+//        /// a w polu <see cref="PlanData.Value"/> - wyznaczony maksymalny zysk fabryki.
+//        /// </returns>
+//        public PlanData CreateComplexPlan(PlanData[] production, PlanData[,] sales, PlanData storageInfo,
+//            out WeeklyPlan[] weeklyPlan)
+//        {
+//            weeklyPlan = null;
+//            return new PlanData
+//            {
+//                Value = 0,
+//                Quantity = 0,
+//            };
+//        }
+//    }
+//}
